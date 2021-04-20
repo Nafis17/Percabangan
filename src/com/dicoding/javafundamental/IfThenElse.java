9f856d9ea1cf0f837add8833afe26f7bbab957b6
Nafis17 <sabitgt17@gmail.com>
2021-04-20 10:44:44 +0800

    Initial commit

---
+++ src/com/dicoding/javafundamental/IfThenElse.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental;
+
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean isOn = false;o
+
+        if (isOn) {
+            System.out.println("Menyalakan Lampu");
+        } else {
+            System.out.println("kondisi tidak Terpenuhi..");
+        }
+    }
+}
